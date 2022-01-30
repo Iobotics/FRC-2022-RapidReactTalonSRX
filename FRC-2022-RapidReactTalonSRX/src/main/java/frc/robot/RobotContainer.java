2c3a1c6b2bbf04b80d9b4642c0dd9d80da8899f4
Josh Lee <itsjosh343@gmail.com>
2022-01-29 17:29:00 -1000

    recent shooter

--- FRC-2022-RapidReactTalonSRX/src/main/java/frc/robot/RobotContainer.java
+++ FRC-2022-RapidReactTalonSRX/src/main/java/frc/robot/RobotContainer.java
@@ -5,23 +5,25 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.OIConstants;
 import frc.robot.subsystems.Drivetrain;
-import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shooter;
+
+
 
 /** Add your docs here. */
 public class RobotContainer {
       // The robot's subsystems and commands are defined here...
 
   private final Drivetrain drivetrain = new Drivetrain();
-  private final Intake intake = new Intake();
   private final Joystick joystick1 = new Joystick(OIConstants.kJoystick1);
   private final Joystick joystick2 = new Joystick(OIConstants.kJoystick2);
-
+  private final Shooter shootLeft = new Shooter();
 
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
@@ -41,10 +43,10 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    new JoystickButton(joystick1, 2).whileHeld(
+    new JoystickButton(joystick2, 2).whileHeld(
         new StartEndCommand(
-          ()-> intake.setPower(joystick1.getZ()),
-          ()-> intake.stop())
+          ()-> shootLeft.setPercent(SmartDashboard.getNumber("DB SLider 0", 0)),
+          ()-> shootLeft.stop())
         );
   }
 
--- FRC-2022-RapidReactTalonSRX/src/main/java/frc/robot/subsystems/Shooter.java
+++ FRC-2022-RapidReactTalonSRX/src/main/java/frc/robot/subsystems/Shooter.java
@@ -4,5 +4,35 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.RobotMap;
+
 /** Add your docs here. */
-public class Shooter {}
+public class Shooter extends SubsystemBase{
+
+    private CANSparkMax shootLeft;
+    private CANSparkMax shootRight;
+
+    public Shooter(){
+        
+        shootLeft = new CANSparkMax(RobotMap.kshootLeft, MotorType.kBrushless);
+        shootRight = new CANSparkMax(RobotMap.kshootRight, MotorType.kBrushless);
+
+        shootRight.follow(shootLeft);
+
+    }
+
+    public void setPercent(double percent){
+
+     shootLeft.set(percent);
+    }
+
+    public void stop(){
+        shootLeft.set(0);
+    }
+    
+}
